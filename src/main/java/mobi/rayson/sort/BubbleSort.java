5abeef48f65c76c1026e09aee9c85af67f10bf8b
lirui <634178463@qq.com>
2018-12-11 21:44:00 +0800

    冒泡排序算法的额外说明

--- src/main/java/mobi/rayson/sort/BubbleSort.java
+++ src/main/java/mobi/rayson/sort/BubbleSort.java
@@ -5,7 +5,11 @@ package mobi.rayson.sort;
  *  User:  lirui
  *  Date:  2018-12-09
  *  Time: 10:06 PM
- *  Description: 排序算法
+ *  Description: 冒泡排序算法。
+ *  比较并交换相邻两个数据大小
+ *  时间复杂度：最好可能 O(n)，最坏可能 O(n^2)
+ *  是原地排序算法：因为空间复杂度为O(1)，只需要常量级的临时内存空间
+ *  是稳定的排序算法：因为两个相同元素在排序后，相对顺序不变
  **/
 public class BubbleSort {
   public static void main(String[] args) {
@@ -16,7 +20,7 @@ public class BubbleSort {
     }
   }
 
-  public static int[] sort(int[] scores) {
+  private static int[] sort(int[] scores) {
     int length = scores.length;
     if (length <= 1) return scores;
     for (int i = 0; i < length; i++) {
@@ -26,10 +30,10 @@ public class BubbleSort {
           int tmp = scores[j];
           scores[j] = scores[j + 1];
           scores[j + 1] = tmp;
-          flag = true;
+          flag = true; // 说明发生了数据交换
         }
       }
-      if (!flag) break;
+      if (!flag) break; // 如果没有发生交换，则说明数据已排好序，直接退出循环
     }
     return scores;
   }
