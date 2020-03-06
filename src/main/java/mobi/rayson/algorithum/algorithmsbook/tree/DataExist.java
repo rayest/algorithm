bf3ea271387c5319e1a18e69ceb6a340ab4b0f3b
李锐 <rayestonal@gmail.com>
2020-03-06 23:20:18 +0800

    data exist

---
+++ src/main/java/mobi/rayson/algorithum/algorithmsbook/tree/DataExist.java
@@ -0,0 +1,28 @@
+package mobi.rayson.algorithum.algorithmsbook.tree;
+
+import java.util.TreeMap;
+
+/**
+ * @author lirui
+ * 给定的值是否存在于二叉树中
+ */
+public class DataExist {
+    public static void main(String[] args) {
+        BinaryTreeNode binaryTreeNode = BaseBinaryTree.initBinary();
+        System.out.println(isDataExist(2, binaryTreeNode));
+        System.out.println(isDataExist(8, binaryTreeNode));
+    }
+
+    private static boolean isDataExist(int data, BinaryTreeNode binaryTreeNode) {
+        if (binaryTreeNode == null) {
+            return false;
+        }
+        if (data == binaryTreeNode.getData()) {
+            return true;
+        }
+        if (isDataExist(data, binaryTreeNode.getLeft())){
+            return true;
+        }
+        return isDataExist(data, binaryTreeNode.getRight());
+    }
+}
