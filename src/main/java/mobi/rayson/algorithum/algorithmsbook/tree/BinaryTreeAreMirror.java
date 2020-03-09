2b4b3c33e002e5826dc778a9c8cb2004b1551511
李锐 <rayestonal@gmail.com>
2020-03-09 17:12:06 +0800

    是否是镜像

---
+++ src/main/java/mobi/rayson/algorithum/algorithmsbook/tree/BinaryTreeAreMirror.java
@@ -0,0 +1,23 @@
+package mobi.rayson.algorithum.algorithmsbook.tree;
+
+/**
+ * @author lirui
+ * 判断两个树是否互为镜像
+ */
+public class BinaryTreeAreMirror {
+    public static void main(String[] args) {
+        System.out.println(areMirror(BaseBinaryTree.initBinary(), BaseBinaryTree.initBinary()));
+    }
+
+    private static boolean areMirror(BinaryTreeNode tree1, BinaryTreeNode tree2) {
+        if (tree1 == null && tree2 == null) {
+            return true;
+        }
+        if (tree1 == null || tree2 == null) {
+            return false;
+        }
+        return (tree1.getData() == tree2.getData())
+                && areMirror(tree1.getLeft(), tree2.getRight())
+                && areMirror(tree1.getRight(), tree2.getLeft());
+    }
+}
