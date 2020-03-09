edb3fe40304c106937df5755aaaa35348cec4e07
李锐 <rayestonal@gmail.com>
2020-03-09 13:58:50 +0800

    count nodes with queue

--- src/main/java/mobi/rayson/algorithum/algorithmsbook/tree/NodeStatistics.java
+++ src/main/java/mobi/rayson/algorithum/algorithmsbook/tree/NodeStatistics.java
@@ -1,5 +1,10 @@
 package mobi.rayson.algorithum.algorithmsbook.tree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
 /**
  * @author lirui
  * 统计节点数
@@ -7,10 +12,31 @@ package mobi.rayson.algorithum.algorithmsbook.tree;
 public class NodeStatistics {
     public static void main(String[] args) {
         System.out.println(countNodes(BaseBinaryTree.initBinary()));
+        System.out.println(countNodesWithNoRecursion(BaseBinaryTree.initBinary()));
+    }
+
+    private static int countNodesWithNoRecursion(BinaryTreeNode binaryTree) {
+        if (binaryTree == null) {
+            return 0;
+        }
+        Queue<BinaryTreeNode> queue = new LinkedList<>();
+        queue.offer(binaryTree);
+        int count = 0;
+        while (!queue.isEmpty()) {
+            count++;
+            BinaryTreeNode temp = queue.poll();
+            if (temp.getLeft() != null) {
+                queue.offer(temp.getLeft());
+            }
+            if (temp.getRight() != null) {
+                queue.offer(temp.getRight());
+            }
+        }
+        return count;
     }
 
     private static int countNodes(BinaryTreeNode binaryTreeNode) {
-        if (binaryTreeNode == null){
+        if (binaryTreeNode == null) {
             return 0;
         }
         return countNodes(binaryTreeNode.getLeft()) + 1 + countNodes(binaryTreeNode.getRight());
