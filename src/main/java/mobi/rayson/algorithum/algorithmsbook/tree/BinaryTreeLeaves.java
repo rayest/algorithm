bd6d10927209ca24d5367851bc67f050b272cdc2
李锐 <rayestonal@gmail.com>
2020-03-09 15:37:28 +0800

    find Deepest Node

---
+++ src/main/java/mobi/rayson/algorithum/algorithmsbook/tree/BinaryTreeLeaves.java
@@ -0,0 +1,37 @@
+package mobi.rayson.algorithum.algorithmsbook.tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author lirui
+ * 统计树的叶子节点数
+ */
+public class BinaryTreeLeaves {
+    public static void main(String[] args) {
+        System.out.println(countLeaves(BaseBinaryTree.initBinary()));
+    }
+
+    private static int countLeaves(BinaryTreeNode binaryTreeNode) {
+        if (binaryTreeNode == null) {
+            return 0;
+        }
+        int count = 0;
+        Queue<BinaryTreeNode> queue = new LinkedList<>();
+        queue.offer(binaryTreeNode);
+        while (!queue.isEmpty()) {
+            BinaryTreeNode treeNode = queue.poll();
+            if (treeNode.getLeft() == null && treeNode.getRight() == null) {
+                count++;
+            } else {
+                if (treeNode.getLeft() != null) {
+                    queue.offer(treeNode.getLeft());
+                }
+                if (treeNode.getRight() != null) {
+                    queue.offer(treeNode.getRight());
+                }
+            }
+        }
+        return count;
+    }
+}
