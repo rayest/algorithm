bfa1d1a25727967f7562a4ce88507219eb20d939
李锐 <rayestonal@gmail.com>
2020-03-09 16:52:06 +0800

    判断是否存在一条路径，其上节点值总和等于给定值

---
+++ src/main/java/mobi/rayson/algorithum/algorithmsbook/tree/BinaryTreeHasPathSum.java
@@ -0,0 +1,20 @@
+package mobi.rayson.algorithum.algorithmsbook.tree;
+
+/**
+ * @author lirui
+ * 是否存在一条路径，使得路径上各个节点值的总和等于给定值
+ */
+public class BinaryTreeHasPathSum {
+    public static void main(String[] args) {
+        System.out.println(hasPathSum(BaseBinaryTree.initBinary(), 10));
+        System.out.println(hasPathSum(BaseBinaryTree.initBinary(), 8));
+    }
+
+    private static boolean hasPathSum(BinaryTreeNode binaryTreeNode, int sum) {
+        if (binaryTreeNode == null) {
+            return sum == 0;
+        }
+        int subSum = sum - binaryTreeNode.getData();
+        return hasPathSum(binaryTreeNode.getLeft(), subSum) || hasPathSum(binaryTreeNode.getRight(), subSum);
+    }
+}
