c78a15c3fb73799a4270dcd418cf232f466933cc
李锐 <rayestonal@gmail.com>
2020-03-09 15:06:32 +0800

    树高

---
+++ src/main/java/mobi/rayson/algorithum/algorithmsbook/tree/BinaryTreeHeight.java
@@ -0,0 +1,20 @@
+package mobi.rayson.algorithum.algorithmsbook.tree;
+
+/**
+ * @author lirui
+ * 统计树高/深度
+ */
+public class BinaryTreeHeight {
+    public static void main(String[] args) {
+        System.out.println(binaryTreeHeight(BaseBinaryTree.initBinary()));
+    }
+
+    private static int binaryTreeHeight(BinaryTreeNode binaryTreeNode) {
+        if (binaryTreeNode == null) {
+            return 0;
+        }
+        int leftHeight = binaryTreeHeight(binaryTreeNode.getLeft());
+        int rightHeight = binaryTreeHeight(binaryTreeNode.getRight());
+        return leftHeight > rightHeight ? leftHeight + 1 : rightHeight + 1;
+    }
+}
